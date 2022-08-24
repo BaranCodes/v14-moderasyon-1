48a7ec80868e3d55ee2237ff3ed2e3878c4e323e
Ly3ssia <93944142+Ly3ssia@users.noreply.github.com>
2022-08-24 19:23:17 +0300

    Update kullanıcı-bilgi.js

--- commands/kullanıcı-bilgi.js
+++ commands/kullanıcı-bilgi.js
@@ -7,11 +7,12 @@ moment.locale("tr");
 exports.run = (client, message, args) => {
  var member = message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.author;
   const embed = new Discord.EmbedBuilder() 
+
   .setDescription(`**➥ Kullanıcı Bilgileri**
         
 • Kullanıcı: (<@${member.id}> - \`${member.id}\`) (${message.member.roles.highest})
-• Hesap Kurulum Tarihi: \`${moment(message.member.createdAt).format('D MMMM YYYY')}\`
-• Sunucuya Katılma Tarihi: \`${moment(message.member.joinedAt).format('D MMMM YYYY')}\`
+• Hesap Kurulum Tarihi: \`${moment(member.createdAt).format('D MMMM YYYY')}\`
+• Sunucuya Katılma Tarihi: \`${moment(member.joinedAt).format('D MMMM YYYY')}\`
 `)
   message.reply({ embeds: [embed]})
   
